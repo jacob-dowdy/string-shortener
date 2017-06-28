4f4ab0d1dfc54517a9da6bb9eda5120fae40102b
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-06-27 21:22:16 -0400

    Update .js

--- .js
+++ .js
@@ -1,7 +1,7 @@
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   if (str.length > num && num > 3) {
-    return str.slice(0, (num - 3)) + '...';
+    return str.slice(0, (num - 2)) + '...';
   } else if (str.length > num && num <= 3) {
     return str.slice(0, num) + '...';
   } else {
